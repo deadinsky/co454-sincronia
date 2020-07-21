2b8a732fcb9c705733fcfb335dd14d868d1c4700
tdedinsk <thomas.dedinsky@uwaterloo.ca>
2020-07-21 15:46:07 -0400

    Added build script for local version

---
+++ LocalJobClass/Job.java
@@ -0,0 +1,13 @@
+//simple triple
+public class Job {
+    public int id;
+    public String egress;
+    public int timeUnits;
+    public int epsilon;
+    Job (int _id, String _egress, int _timeUnits, int _epsilon) {
+        id = _id;
+        egress = _egress;
+        timeUnits = _timeUnits;
+        epsilon = _epsilon;
+    }
+}
